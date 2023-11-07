9e480371479f4c63ec3e5d203a2b6d1399c04f22
Igor <igorrybalko2009@gmail.com>
2023-11-07 20:13:14 +0200

    fix ts

--- src/views/Base64EncodePage.tsx
+++ src/views/Base64EncodePage.tsx
@@ -34,8 +34,8 @@ export default function Base64EncodePage() {
   return btoa(
    encodeURIComponent(str).replace(
     /%([0-9A-F]{2})/g,
-    function toSolidBytes(match, p1) {
-     return String.fromCharCode('0x' + p1);
+    function toSolidBytes(_match, p1) {
+     return String.fromCharCode(parseInt('0x' + p1, 16));
     }
    )
   );
