794493f4402fc16b3d5b98afa002d46d7f0a0406
Igor <igorrybalko2009@gmail.com>
2024-03-26 21:34:32 +0200

    added img to base64

--- src/views/base64/ImgToBase64Page.tsx
+++ src/views/base64/ImgToBase64Page.tsx
@@ -22,6 +22,8 @@ import { useLoadPage } from '../../hooks';
 
 const { Option } = Select;
 
+type ConvertType = 'simple' | 'dataUri';
+
 export default function ImgToBase64Page() {
  const [result, setResult] = useState('');
  const [api, contextHolder] = notification.useNotification();
@@ -39,14 +41,28 @@ export default function ImgToBase64Page() {
   },
  ];
 
- const onFinish = (val: { upload: UploadFile[] }) => {
-  if (val.upload) {
-   try {
-    const str = val.upload[0].thumbUrl;
+ const converter = {
+  simple(str: string) {
+   const arr = str.split(';base64,');
+   return arr[1];
+  },
+  dataUri(str: string) {
+   return str;
+  },
+ };
 
-    if (str) {
-     setResult(str);
-    }
+ const onFinish = (val: { upload: UploadFile[]; format: ConvertType }) => {
+  if (val.upload && val.upload[0]) {
+   try {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+     const rawStr = reader.result;
+     if (rawStr) {
+      const str = converter[val.format](rawStr as string);
+      setResult(str);
+     }
+    };
+    reader.readAsDataURL(val.upload[0].originFileObj as File);
    } catch (err) {
     console.log(err);
     api.error({
@@ -71,6 +87,14 @@ export default function ImgToBase64Page() {
  };
 
  const normFile = (e: any) => {
+  if (e.file.size > 9441674) {
+   api.error({
+    message: 'Error',
+    description: 'File too big',
+   });
+   return null;
+  }
+
   if (Array.isArray(e)) {
    return e;
   }
@@ -80,11 +104,10 @@ export default function ImgToBase64Page() {
  return (
   <div>
    <Helmet>
-    <title>Base64 Encode Online Tools - Free</title>
-    <meta
-     name='description'
-     content='Encode string to Base64 format online - free. Convert text to Base64'
-    />
+    <title>
+     Image to Base64 Converter Online | Base64 Image Encoder (Free)
+    </title>
+    <meta name='description' content='Convert an image to Base64 online. Conver jpg, png, svg, webp to Base64' />
     <link
      rel='canonical'
      href={import.meta.env.VITE_SITE_URL + '/img-to-base64'}
@@ -93,7 +116,7 @@ export default function ImgToBase64Page() {
    {contextHolder}
    <Row gutter={[24, 0]}>
     <Col xs={24} sm={24} md={18}>
-     <h1>Base64 Encode Online (Text)</h1>
+     <h1>Convert Image to Base64 Online</h1>
      <Form
       onFinish={onFinish}
       autoComplete='off'
@@ -110,7 +133,7 @@ export default function ImgToBase64Page() {
         name='img'
         listType='picture'
         maxCount={1}
-        accept='image/png, image/jpeg'
+        accept='image/png, image/jpeg, image/svg+xml, image/webp'
         beforeUpload={() => {
          /* update state here */
          return false;
@@ -121,7 +144,7 @@ export default function ImgToBase64Page() {
       </Form.Item>
 
       <Form.Item name='format' label='Output Format'>
-       <Select placeholder='Please select a country'>
+       <Select>
         <Option value='simple'>Only Base64</Option>
         <Option value='dataUri'>Data URI</Option>
        </Select>
@@ -146,23 +169,38 @@ export default function ImgToBase64Page() {
      </Button>
      <Divider />
      <div>
-      <h2>Encode a string to Base64 online</h2>
+      <h2>Encode your image to Base64 online</h2>
       <p>
-       Encoding is done using the{' '}
-       <a
-        href='https://developer.mozilla.org/en-US/docs/Web/API/btoa'
-        target='_blank'
-        rel='nofollow'
-       >
-        btoa()
-       </a>{' '}
-       function
+       You can upload an image in next formats:{' '}
+       <strong>jpg, png, svg, webp</strong>. Maximum image size{' '}
+       <strong>9 MB</strong>.
       </p>
+      <p>We don't save your images on our server.</p>
       <p>
-       Base64 is a standard for encoding binary data using only 64 ASCII
-       characters. The encoding uses Latin characters A-Z, a-z, numbers 0-9 (62
-       characters in total) and 2 additional characters
+       Once you have a Base64 string representation of an image, you can use it
+       in various ways depending on your specific requirements and the
+       environment you're working in:
       </p>
+      <ul>
+       <li>
+        <strong>Embedding in HTML:</strong> You can directly embed the Base64
+        string into HTML using the img tag with the src attribute set to the
+        Base64 string. This allows you to display the image on a webpage without
+        needing a separate image file.
+       </li>
+       <li>
+        <strong>Storing in Databases:</strong> If you're working with databases,
+        you can store the Base64 string as a text. This allows you to
+        efficiently store and retrieve image data without needing to handle
+        separate image files.
+       </li>
+       <li>
+        <strong>Sending via APIs:</strong> When working with APIs, you can
+        include the Base64 string as part of the payload when sending data to a
+        server. This enables you to transmit image data along with other
+        information in a single request.
+       </li>
+      </ul>
      </div>
     </Col>
     <Col xs={24} sm={24} md={6}>
