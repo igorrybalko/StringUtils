4547cbe631896bd025cd7da428efca89af32ef7c
Igor <igorrybalko2009@gmail.com>
2024-07-22 17:43:45 +0300

    added hotjar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import Hotjar from '@hotjar/browser';
 
 import routes from './router';
 
+const siteId = 5067838;
+const hotjarVersion = 6;
+
 function App() {
  //const { darkAlgorithm } = theme;
+
+ Hotjar.init(siteId, hotjarVersion);
+
  return (
   <ConfigProvider
    theme={{
